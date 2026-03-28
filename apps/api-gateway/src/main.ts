73cc6cb0885921108a92ce4d7a8ccb6372fcfe34
dnchuong17 <chuongdo172@gmail.com>
2026-03-28 21:13:48 +0700

    add port

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -9,7 +9,14 @@ async function bootstrap() {
   logger.setServiceName("Api-gateway");
   logger.info("API Gateway is starting...");
   app.enableCors({
-    origin: ["*", "http://172.16.25.252:3000", "http://172.16.25.252:3001"],
+    origin: [
+      "http://172.16.25.252:3000",
+      "http://172.16.25.252:3001",
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://vietflood-fe.vercel.app/",
+      "https://viet-flood-app.vercel.app/",
+    ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });

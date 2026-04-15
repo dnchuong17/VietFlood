560f38179f6db3db94ea0dbc20b58a451248870c
dnchuong17 <chuongdo172@gmail.com>
2026-04-15 20:07:22 +0700

    mdify port

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -9,14 +9,7 @@ async function bootstrap() {
   logger.setServiceName("Api-gateway");
   logger.info("API Gateway is starting...");
   app.enableCors({
-    origin: [
-      "http://172.16.25.252:3000",
-      "http://172.16.25.252:3001",
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "https://vietflood-fe.vercel.app",
-      "https://viet-flood-app.vercel.app",
-    ],
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
